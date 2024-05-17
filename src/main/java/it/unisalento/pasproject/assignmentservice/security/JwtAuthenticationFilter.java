f2b30fe27aefbfab8ef52cf189e58af2dedf70e8
Fediz01 <federico.izzi@studenti.unisalento.it>
2024-05-17 22:58:23 +0200

    Minor fixes

--- src/main/java/it/unisalento/pasproject/assignmentservice/security/JwtAuthenticationFilter.java
+++ src/main/java/it/unisalento/pasproject/assignmentservice/security/JwtAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package it.unisalento.pasproject.assignmentservice.security;
 
 
+import it.unisalento.pasproject.assignmentservice.exceptions.AccessDeniedException;
+import it.unisalento.pasproject.assignmentservice.exceptions.UserNotAuthorizedException;
 import it.unisalento.pasproject.assignmentservice.service.UserCheckService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -26,17 +28,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-            throws ServletException, IOException {
+            throws ServletException, IOException, UserNotAuthorizedException {
 
         final String authorizationHeader = request.getHeader("Authorization");
 
         String username = null;
         String jwt = null;
 
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            jwt = authorizationHeader.substring(7);
-            username = jwtUtilities.extractUsername(jwt);
+        try {
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                jwt = authorizationHeader.substring(7);
+                username = jwtUtilities.extractUsername(jwt);
+            }
+        } catch (Exception e) {
+            throw new AccessDeniedException("Invalid token");
         }
+
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetailsDTO user = this.userCheckService.loadUserByUsername(username);
@@ -52,6 +59,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 usernamePasswordAuthenticationToken
                         .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            } else {
+                throw new UserNotAuthorizedException("User not authorized");
             }
         }
 
