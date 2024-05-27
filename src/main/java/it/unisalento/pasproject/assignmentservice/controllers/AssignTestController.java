f7893eeb16170370aac10fb6da08525119ab9c85
mirkocaforio <mirkocaforio2002@gmail.com>
2024-05-27 17:06:03 +0200

    Fix AssignTestController

--- src/main/java/it/unisalento/pasproject/assignmentservice/controllers/AssignTestController.java
+++ src/main/java/it/unisalento/pasproject/assignmentservice/controllers/AssignTestController.java
@@ -1,8 +1,8 @@
 package it.unisalento.pasproject.assignmentservice.controllers;
 
 import it.unisalento.pasproject.assignmentservice.domain.TaskAssignment;
-import it.unisalento.pasproject.assignmentservice.repositories.AssignedResourceRepository;
 import it.unisalento.pasproject.assignmentservice.repositories.TaskAssignmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/test/assignment")
 public class AssignTestController {
-    private AssignedResourceRepository assignedResourceRepository;
+    @Autowired
     private TaskAssignmentRepository taskAssignmentRepository;
 
     @GetMapping(value="/tasks")
