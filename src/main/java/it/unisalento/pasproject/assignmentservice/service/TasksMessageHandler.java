c52f5ed1990dfd5684b08c2019b4cd21193a5c13
fizzi01 <federico.izzi@studenti.unisalento.it>
2024-06-19 11:31:07 +0200

    fix: Minor adjustments

--- src/main/java/it/unisalento/pasproject/assignmentservice/service/TasksMessageHandler.java
+++ src/main/java/it/unisalento/pasproject/assignmentservice/service/TasksMessageHandler.java
@@ -6,6 +6,8 @@ import it.unisalento.pasproject.assignmentservice.domain.Task;
 import it.unisalento.pasproject.assignmentservice.dto.task.TaskMessageDTO;
 import it.unisalento.pasproject.assignmentservice.dto.task.TaskStatusMessageDTO;
 import it.unisalento.pasproject.assignmentservice.repositories.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -33,14 +35,18 @@ public class TasksMessageHandler {
     @Value("${rabbitmq.exchange.data.name}")
     private String dataExchange;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TasksMessageHandler.class);
+
 
     private final MessageProducer messageProducer;
     private final TaskRepository taskRepository;
+    private final AllocationService allocationService;
 
     @Autowired
-    public TasksMessageHandler(TaskRepository taskRepository, MessageProducer messageProducer, @Qualifier("RabbitMQProducer") MessageProducerStrategy strategy) {
+    public TasksMessageHandler(TaskRepository taskRepository, MessageProducer messageProducer, @Qualifier("RabbitMQProducer") MessageProducerStrategy strategy, AllocationService allocationService) {
         this.taskRepository = taskRepository;
         this.messageProducer = messageProducer;
+        this.allocationService = allocationService;
         messageProducer.setStrategy(strategy);
     }
 
@@ -51,44 +57,58 @@ public class TasksMessageHandler {
     @RabbitListener(queues = "${rabbitmq.queue.newtask.name}")
     public void handleNewTask(TaskMessageDTO taskMessageDTO) {
 
-        Optional<Task> task = taskRepository.findByIdTask(taskMessageDTO.getId());
+        try {
+            Optional<Task> task = taskRepository.findByIdTask(taskMessageDTO.getId());
 
-        Task newTask = new Task();
+            Task newTask = new Task();
 
-        // If the task is already present in the database, update it
-        if(task.isPresent()) {
-            newTask = task.get();
-        }
-
-        //Non viene controllato se manca qualche campo, perchè il TaskManager dovrebbe inviare sempre tutti i campi
-        //anche quando la task viene aggiornata. SOLO DATA CONSITENCY
-
-        newTask.setIdTask(taskMessageDTO.getId());
-        newTask.setEmailUtente(taskMessageDTO.getEmailUtente());
-        newTask.setMaxComputingPower(taskMessageDTO.getMaxComputingPower());
-        newTask.setMaxCudaPower(taskMessageDTO.getMaxCudaPower());
-        newTask.setMinCudaPower(taskMessageDTO.getMinCudaPower());
-        newTask.setMinComputingPower(taskMessageDTO.getMinComputingPower());
-        newTask.setTaskDuration(taskMessageDTO.getTaskDuration());
-        newTask.setMaxEnergyConsumption(taskMessageDTO.getMaxEnergyConsumption());
-        newTask.setMinEnergyConsumption(taskMessageDTO.getMinEnergyConsumption());
-        newTask.setMinWorkingTime(taskMessageDTO.getMinWorkingTime());
-        newTask.setRunning(taskMessageDTO.getRunning());
-        newTask.setEnabled(taskMessageDTO.getEnabled());
-
-        if (newTask.getEnabled() && newTask.getRunning()) {
-            if (newTask.getStartTime() == null) {
-                newTask.setStartTime(LocalDateTime.now());
-                newTask.setEndTime(newTask.getStartTime().plusSeconds(newTask.getTaskDuration().longValue()));
+            // If the task is already present in the database, update it
+            if (task.isPresent()) {
+                newTask = task.get();
             }
-        } else {
-            newTask.setStartTime(null);
-        }
 
-        taskRepository.save(newTask);
+            //Non viene controllato se manca qualche campo, perchè il TaskManager dovrebbe inviare sempre tutti i campi
+            //anche quando la task viene aggiornata. SOLO DATA CONSITENCY
+
+            newTask.setIdTask(taskMessageDTO.getId());
+            newTask.setEmailUtente(taskMessageDTO.getEmailUtente());
+            newTask.setMaxComputingPower(taskMessageDTO.getMaxComputingPower());
+            newTask.setMaxCudaPower(taskMessageDTO.getMaxCudaPower());
+            newTask.setMinCudaPower(taskMessageDTO.getMinCudaPower());
+            newTask.setMinComputingPower(taskMessageDTO.getMinComputingPower());
+            newTask.setTaskDuration(taskMessageDTO.getTaskDuration());
+            newTask.setMaxEnergyConsumption(taskMessageDTO.getMaxEnergyConsumption());
+            newTask.setMinEnergyConsumption(taskMessageDTO.getMinEnergyConsumption());
+            newTask.setMinWorkingTime(taskMessageDTO.getMinWorkingTime());
+            newTask.setRunning(taskMessageDTO.getRunning());
+            newTask.setEnabled(taskMessageDTO.getEnabled());
+
+            if (newTask.getEnabled() && newTask.getRunning()) {
+                if (newTask.getStartTime() == null) {
+                    newTask.setStartTime(LocalDateTime.now());
+                    newTask.setEndTime(newTask.getStartTime().plusSeconds(newTask.getTaskDuration().longValue()));
+                }
+            } else {
+                newTask.setStartTime(null);
+            }
+
+            // If the task is forced to stop, deallocate all the resources without Watcher
+            if (Boolean.FALSE.equals(newTask.getRunning())) {
+                LOGGER.warn("Task {} is forced to stop", newTask.getId());
+                newTask.setEndTime(LocalDateTime.now());
+                deallocateAllResources(newTask);
+            }
+
+            taskRepository.save(newTask);
+        }catch (Exception e){
+            LOGGER.error("Error while saving task : {}", e.getMessage());
+        }
     }
 
-
+    private void deallocateAllResources(Task task) {
+        //Dealloca tutte le risorse assegnate alla task
+        allocationService.deallocateResources(allocationService.getActiveTaskAssignment(task.getId()));
+    }
 
     /**
      * This method is called when a user is assigned to a task
