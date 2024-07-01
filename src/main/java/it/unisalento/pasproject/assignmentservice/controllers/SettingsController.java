85f7a28c08f966b8909398f2937c7e8e4e854f7e
fizzi01 <federico.izzi@studenti.unisalento.it>
2024-07-01 08:05:31 +0200

    fix: Settings limited to role Admin

--- src/main/java/it/unisalento/pasproject/assignmentservice/controllers/SettingsController.java
+++ src/main/java/it/unisalento/pasproject/assignmentservice/controllers/SettingsController.java
@@ -3,9 +3,12 @@ package it.unisalento.pasproject.assignmentservice.controllers;
 import it.unisalento.pasproject.assignmentservice.business.CheckOutUtils;
 import it.unisalento.pasproject.assignmentservice.dto.SettingsDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static it.unisalento.pasproject.assignmentservice.security.SecurityConstants.ROLE_ADMIN;
 
 @RestController
 @RequestMapping("/api/assignments/settings")
@@ -19,11 +22,13 @@ public class SettingsController {
     }
 
     @GetMapping("/get")
+    @Secured({ROLE_ADMIN})
     public SettingsDTO getSettings() {
        return checkOutUtils.getSettingsDTO(checkOutUtils.getSettings());
     }
 
     @GetMapping("/set")
+    @Secured({ROLE_ADMIN})
     public SettingsDTO setSettings(SettingsDTO settingsDTO) {
         return checkOutUtils.getSettingsDTO(checkOutUtils.setSettings(checkOutUtils.getSettingsFromDTO(settingsDTO)));
     }
